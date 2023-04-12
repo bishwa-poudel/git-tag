75f141255173ccbfa9254e47c81827b5ceb68412
Bishwa Poudel <bispoul181@gmail.com>
2023-04-12 08:56:15 +0545

    Generate new tag based on previous tag

--- git-tag.sh
+++ git-tag.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Define the tag name and commit message
-TAG_NAME="v1.0.0"
-COMMIT_MESSAGE="Release version ${TAG_NAME}"
+# Get the previous tag name
+PREVIOUS_TAG=$(git describe --tags --abbrev=0)
+
+# Split the previous tag into its components
+IFS='.' read -ra PREVIOUS_TAG_COMPONENTS <<< "${PREVIOUS_TAG}"
+
+# Increment the last component of the previous tag to get the new tag
+NEW_TAG="${PREVIOUS_TAG_COMPONENTS[0]}.${PREVIOUS_TAG_COMPONENTS[1]}.$((PREVIOUS_TAG_COMPONENTS[2]+1))"
+
+# Define the commit message
+COMMIT_MESSAGE="Release version ${NEW_TAG}"
 
 # Create the tag and push it to the remote repository
-git tag -a "${TAG_NAME}" -m "${COMMIT_MESSAGE}"
-git push origin "${TAG_NAME}"
+git tag -a "${NEW_TAG}" -m "${COMMIT_MESSAGE}"
+git push origin "${NEW_TAG}"
