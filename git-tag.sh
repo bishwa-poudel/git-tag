de77471a6f094037dbb3694f0381cab4b95a6841
Bishwa Poudel <bispoul181@gmail.com>
2023-04-12 08:57:39 +0545

    Generate tag according to TMS Release pattern

--- git-tag.sh
+++ git-tag.sh
@@ -4,10 +4,19 @@
 PREVIOUS_TAG=$(git describe --tags --abbrev=0)
 
 # Split the previous tag into its components
-IFS='.' read -ra PREVIOUS_TAG_COMPONENTS <<< "${PREVIOUS_TAG}"
+IFS='-' read -ra PREVIOUS_TAG_COMPONENTS <<< "${PREVIOUS_TAG}"
 
-# Increment the last component of the previous tag to get the new tag
-NEW_TAG="${PREVIOUS_TAG_COMPONENTS[0]}.${PREVIOUS_TAG_COMPONENTS[1]}.$((PREVIOUS_TAG_COMPONENTS[2]+1))"
+# Extract the version number from the previous tag
+PREVIOUS_VERSION="${PREVIOUS_TAG_COMPONENTS[2]}"
+
+# Split the previous version number into its components
+IFS='.' read -ra PREVIOUS_VERSION_COMPONENTS <<< "${PREVIOUS_VERSION}"
+
+# Increment the last component of the previous version number to get the new version number
+NEW_VERSION="${PREVIOUS_VERSION_COMPONENTS[0]}.${PREVIOUS_VERSION_COMPONENTS[1]}.$((PREVIOUS_VERSION_COMPONENTS[2]+1))"
+
+# Define the new tag name
+NEW_TAG="TMS-CLIENT-V${NEW_VERSION}-$(date +%Y-%m-%d)"
 
 # Define the commit message
 COMMIT_MESSAGE="Release version ${NEW_TAG}"
